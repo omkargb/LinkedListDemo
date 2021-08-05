66b541908055dd6bf59383e930700af976568c05
omkargb <omkargb107@gmail.com>
2021-08-05 23:16:59 +0530

    [omkargb] Add : Reverse input linked list of 3 elements

--- LinkedListDemo/LinkedList.cs
+++ LinkedListDemo/LinkedList.cs
@@ -26,5 +26,40 @@ namespace LinkedListDemo
             }
             Console.WriteLine(" Element {0} inserted into the LinkedList. ", node.data);
         }
+
+
+
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine(" This LinkedList is empty");
+                return;
+            }
+            Console.Write("\n Displaying LinkedList : ");
+            while (temp != null)
+            {
+                Console.Write(" → [" + temp.data + "]");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+
     }
 }
--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -8,10 +8,20 @@ namespace LinkedListDemo
         {
             Console.WriteLine(" Welcome to LinkedList program.\n");
             LinkedList list = new LinkedList();
-            //add
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+
+            //Console.WriteLine(" LinkedList Sequence : 56 -> 30 -> 70 ");
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+
+            Console.WriteLine(" Node input Sequence : 70 -> 30 -> 56 \t Output LinkedList Sequence : 56 -> 30 -> 70");
+            //add in reverse order
+            list.AddInReverseOrder(70);
+            list.AddInReverseOrder(30);
+            list.AddInReverseOrder(56);
+            list.Display();
+
         }
     }
 }
