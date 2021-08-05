ee3b8a9f9977aa998fcb4d7458853af39ab682b3
omkargb <omkargb107@gmail.com>
2021-08-05 22:18:07 +0530

    [omkargb] Add : Create linked list of 3 elements

---
+++ LinkedListDemo/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDemo
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else 
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(" Element {0} inserted into the LinkedList. ", node.data);
+        }
+    }
+}
---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to LinkedList program.\n");
+            LinkedList list = new LinkedList();
+            //add
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
